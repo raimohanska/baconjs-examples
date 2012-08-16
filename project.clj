bbc74eb6fa280ccd73461a2da1713d355307cc84
Juha Paananen <juha.paananen@gmail.com>
2012-08-16 21:50:08 +0300

    init commit: hello world app

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject baconjs "1.0.0-SNAPSHOT"
+  :description "Bacon.js site"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "1.1.2"]
+                 [ring/ring-jetty-adapter "1.1.2"]]
+  :dev-dependencies [[ring/ring-devel "0.2.0"]])
