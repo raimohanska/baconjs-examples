0ef5649236434a14f09bd3b70907a6854059152f
Juha Paananen <juha.paananen@gmail.com>
2012-08-27 21:46:32 +0300

    preparing for the AJAX search example

--- src/baconjs/web.clj
+++ src/baconjs/web.clj
@@ -3,10 +3,13 @@
         [hiccup.middleware :only (wrap-base-url)])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [compojure.response :as response]))
+            [compojure.response :as response]
+            [ring.util.response :as resp]))
 
 (defroutes main-routes
-  (GET "/" [] "LOL")
+  (GET "/" [] (resp/redirect "/index.html"))
+  (GET "/search/:query" [query]
+      (str "Search result for "  query))
   (route/resources "/")
   (route/not-found "Page not found"))
 
